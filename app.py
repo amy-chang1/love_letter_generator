a936da3243bab3890641171b628367ed45d87c2d
amy-chang1 <amychang3201@gmail.com>
2024-08-25 18:55:28 -0400

    commit

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def generate_love_letter():
     return render_template('letter.html', letter=letter, name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
